e7713055a7a2bf83c69a551a04f1db323a951cb4
moguon <su2167274@naver.com>
2024-09-13 11:32:35 +0900

    nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -2,7 +2,7 @@
 mkdir /nfs-shared
 chmod 777 /nfs-shared
 echo "/nfs-shared 10.0.0.0/8(rw,sync)" > /etc/exports
-systemctl restatrt nfs-server
+systemctl restart nfs-server
 systemctl enable nfs-server
 ec2-metadata -o | cut -d ' ' -f 2 >> /nfs-shared/list.txt
 chmod 777 /nfs-shared/list.txt
